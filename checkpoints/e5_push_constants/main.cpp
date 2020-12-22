c90b5b0622354a56e0c1e5a3d95bf93029fa5051
Sean Baxter <lightborn@gmail.com>
2020-12-22 12:56:06 -0500

    Ported e5_push_constants

--- checkpoints/e5_push_constants/main.cpp
+++ checkpoints/e5_push_constants/main.cpp
@@ -14,9 +14,207 @@
 #include <nvvk/shaders_vk.hpp>         // For nvvk::createShaderModule
 #include <nvvk/structs_vk.hpp>         // For nvvk::make
 
-#include "common.h"
+static const uint32_t workgroup_width  = 16;
+static const uint32_t workgroup_height = 8;
 
-PushConstants pushConstants{1024 /* render_width */, 600 /* render_height */};
+enum bindings {
+  binding_imageData = 0,
+  binding_tlas      = 1,
+  binding_vertices  = 2,
+  binding_indices   = 3,
+};
+
+struct PushConstants
+{
+  ivec2 resolution;
+  int num_samples;
+};
+
+[[spirv::push]]
+PushConstants shader_push;
+
+[[using spirv: buffer, binding(binding_imageData)]]
+vec3 shader_imageData[];
+
+[[using spirv: uniform, binding(binding_tlas)]]
+accelerationStructure shader_tlas;
+
+[[using spirv: buffer, binding(binding_vertices)]]
+vec3 shader_vertices[];
+
+[[using spirv: buffer, binding(binding_indices)]]
+uint shader_indices[];
+
+// Steps the RNG and returns a floating-point value between 0 and 1 inclusive.
+inline float stepAndOutputRNGFloat(uint& rngState)
+{
+  // Condensed version of pcg_output_rxs_m_xs_32_32, with simple conversion to floating-point [0,1].
+  rngState  = rngState * 747796405 + 1;
+  uint word = ((rngState >> ((rngState >> 28) + 4)) ^ rngState) * 277803737;
+  word      = (word >> 22) ^ word;
+  return float(word) / 4294967295.0f;
+}
+
+
+// Uses the Box-Muller transform to return a normally distributed (centered
+// at 0, standard deviation 1) 2D point.
+vec2 randomGaussian(uint& rngState)
+{
+  // Almost uniform in (0, 1] - make sure the value is never 0:
+  const float u1    = max(1e-38f, stepAndOutputRNGFloat(rngState));
+  const float u2    = stepAndOutputRNGFloat(rngState);  // In [0, 1]
+  const float r     = sqrt(-2.0 * log(u1));
+  const float theta = 2 * M_PIf32 * u2;  // Random in [0, 2pi]
+  return r * vec2(cos(theta), sin(theta));
+}
+
+// Returns the color of the sky in a given direction (in linear color space)
+inline vec3 skyColor(vec3 direction)
+{
+  // +y in world space is up, so:
+  if(direction.y > 0.0f)
+  {
+    return mix(vec3(1.0f), vec3(0.25f, 0.5f, 1.0f), direction.y);
+  }
+  else
+  {
+    return vec3(0.03f);
+  }
+}
+
+struct HitInfo
+{
+  vec3 color;
+  vec3 worldPosition;
+  vec3 worldNormal;
+};
+
+inline HitInfo getObjectHitInfo(gl_rayQuery& rayQuery)
+{
+  HitInfo result;
+  // Get the ID of the triangle
+  const int primitiveID = gl_rayQueryGetIntersectionPrimitiveIndex(rayQuery, true);
+
+  // Get the indices of the vertices of the triangle
+  const uint i0 = shader_indices[3 * primitiveID + 0];
+  const uint i1 = shader_indices[3 * primitiveID + 1];
+  const uint i2 = shader_indices[3 * primitiveID + 2];
+
+  // Get the vertices of the triangle
+  const vec3 v0 = shader_vertices[i0];
+  const vec3 v1 = shader_vertices[i1];
+  const vec3 v2 = shader_vertices[i2];
+
+  // Get the barycentric coordinates of the intersection
+  vec3 barycentrics;
+  barycentrics.yz = gl_rayQueryGetIntersectionBarycentrics(rayQuery, true);
+  barycentrics.x    = 1.0 - barycentrics.y - barycentrics.z;
+
+  // Compute the coordinates of the intersection
+  const vec3 objectPos = v0 * barycentrics.x + v1 * barycentrics.y + v2 * barycentrics.z;
+  // For the main tutorial, object space is the same as world space:
+  result.worldPosition = objectPos;
+
+  const vec3 objectNormal = normalize(cross(v1 - v0, v2 - v0));
+  result.worldNormal = objectNormal;
+
+  result.color = vec3(0.7f);
+  return result;
+}
+
+[[using spirv: comp, local_size(workgroup_width, workgroup_height)]]
+void compute_shader() {
+  const ivec2 resolution = shader_push.resolution;
+  const ivec2 pixel = ivec2(glcomp_GlobalInvocationID.xy);
+
+  if((pixel.x >= resolution.x) || (pixel.y >= resolution.y))
+    return;
+
+  // State of the random number generator.
+  uint rngState = resolution.x * pixel.y + pixel.x;  // Initial seed
+
+  const vec3 cameraOrigin = vec3(-0.001, 1.0, 6.0);
+  const float fovVerticalSlope = 1.0 / 5.0;
+
+  // The sum of the colors of all of the samples.
+  vec3 summedPixelColor (0.0);
+
+  // Limit the kernel to trace at most 64 samples.
+  for(int sampleIdx = 0; sampleIdx < shader_push.num_samples; sampleIdx++)
+  {
+
+    // Rays always originate at the camera for now. In the future, they'll
+    // bounce around the scene.
+    vec3 rayOrigin = cameraOrigin;
+
+    const vec2 randomPixelCenter = vec2(pixel) + vec2(0.5) + 
+      0.375f * randomGaussian(rngState);
+
+    vec2 screenUV = vec2(2 * randomPixelCenter + 1 - vec2(resolution)) / vec2(resolution);
+    screenUV.y = -screenUV.y;
+
+
+    vec3 rayDirection(fovVerticalSlope * screenUV, -1.0);
+    rayDirection = normalize(rayDirection);
+
+    vec3 accumulatedRayColor(1);  // The amount of light that made it to the end of the current ray.
+
+
+    // Limit the kernel to trace at most 32 segments.
+    for(int tracedSegments = 0; tracedSegments < 32; tracedSegments++)
+    {
+      // Trace the ray and see if and where it intersects the scene!
+      // First, initialize a ray query object:
+      gl_rayQuery rayQuery;
+      gl_rayQueryInitialize(rayQuery,              // Ray query
+                            shader_tlas,           // Top-level acceleration structure
+                            gl_RayFlagsOpaque,     // Ray flags, here saying "treat all geometry as opaque"
+                            0xFF,                  // 8-bit instance mask, here saying "trace against all instances"
+                            rayOrigin,             // Ray origin
+                            0.0,                   // Minimum t-value
+                            rayDirection,          // Ray direction
+                            10000.0);              // Maximum t-value
+      while(gl_rayQueryProceed(rayQuery));
+
+      // Get the type of committed (true) intersection - nothing, a triangle, or
+      // a generated object
+      if(gl_rayQueryGetIntersectionType(rayQuery, true) == 
+        gl_RayQueryCommittedIntersectionTriangle)
+      {
+        // Ray hit a triangle
+        HitInfo hitInfo = getObjectHitInfo(rayQuery);
+
+        // Apply color absorption
+        accumulatedRayColor *= hitInfo.color;
+
+        // Flip the normal so it points against the ray direction:
+        hitInfo.worldNormal = faceforward(hitInfo.worldNormal, rayDirection, hitInfo.worldNormal);
+
+        // Start a new ray at the hit position, but offset it slightly along the normal:
+        rayOrigin = hitInfo.worldPosition + 0.0001f * hitInfo.worldNormal;
+
+        // Reflect the direction of the ray using the triangle normal:
+        rayDirection = reflect(rayDirection, hitInfo.worldNormal);
+      }
+      else
+      {
+        // Ray hit the sky
+        accumulatedRayColor *= skyColor(rayDirection);
+
+        // Sum this with the pixel's other samples.
+        // (Note that we treat a ray that didn't find a light source as if it had
+        // an accumulated color of (0, 0, 0)).
+        summedPixelColor += accumulatedRayColor;
+
+        break;
+      }
+    }
+  }
+
+  // Get the index of this invocation in the buffer:
+  uint linearIndex       = resolution.x * pixel.y + pixel.x;
+  shader_imageData[linearIndex] =  summedPixelColor / shader_push.num_samples;
+}
 
 VkCommandBuffer AllocateAndBeginOneTimeCommandBuffer(VkDevice device, VkCommandPool cmdPool)
 {
@@ -73,7 +271,15 @@ int main(int argc, const char** argv)
   allocator.init(context, context.m_physicalDevice);
 
   // Create a buffer
-  VkDeviceSize bufferSizeBytes = uint64_t(pushConstants.render_width) * uint64_t(pushConstants.render_height) * 3 * sizeof(float);
+  PushConstants pushConstants {
+    1024 /* render_width */, 
+    600 /* render_height */,
+    64  /* num samples */
+  };
+
+  VkDeviceSize bufferSizeBytes = 
+    uint64_t(pushConstants.resolution.x) * 
+    uint64_t(pushConstants.resolution.y) * 3 * sizeof(float);
   VkBufferCreateInfo bufferCreateInfo = nvvk::make<VkBufferCreateInfo>();
   bufferCreateInfo.size               = bufferSizeBytes;
   bufferCreateInfo.usage              = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
@@ -182,10 +388,10 @@ int main(int argc, const char** argv)
   // 2 - a storage buffer (the vertex buffer)
   // 3 - a storage buffer (the index buffer)
   nvvk::DescriptorSetContainer descriptorSetContainer(context);
-  descriptorSetContainer.addBinding(BINDING_IMAGEDATA, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT);
-  descriptorSetContainer.addBinding(BINDING_TLAS, VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR, 1, VK_SHADER_STAGE_COMPUTE_BIT);
-  descriptorSetContainer.addBinding(BINDING_VERTICES, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT);
-  descriptorSetContainer.addBinding(BINDING_INDICES, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT);
+  descriptorSetContainer.addBinding(binding_imageData, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT);
+  descriptorSetContainer.addBinding(binding_tlas, VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR, 1, VK_SHADER_STAGE_COMPUTE_BIT);
+  descriptorSetContainer.addBinding(binding_vertices, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT);
+  descriptorSetContainer.addBinding(binding_indices, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT);
   // Create a layout from the list of bindings
   descriptorSetContainer.initLayout();
   // Create a descriptor pool from the list of bindings with space for 1 set, and allocate that set
@@ -206,37 +412,37 @@ int main(int argc, const char** argv)
   VkDescriptorBufferInfo descriptorBufferInfo{};
   descriptorBufferInfo.buffer = buffer.buffer;    // The VkBuffer object
   descriptorBufferInfo.range  = bufferSizeBytes;  // The length of memory to bind; offset is 0.
-  writeDescriptorSets[0] = descriptorSetContainer.makeWrite(0 /*set index*/, BINDING_IMAGEDATA /*binding*/, &descriptorBufferInfo);
+  writeDescriptorSets[0] = descriptorSetContainer.makeWrite(0 /*set index*/, binding_imageData /*binding*/, &descriptorBufferInfo);
   // Top-level acceleration structure (TLAS)
   VkWriteDescriptorSetAccelerationStructureKHR descriptorAS = nvvk::make<VkWriteDescriptorSetAccelerationStructureKHR>();
   VkAccelerationStructureKHR tlasCopy = raytracingBuilder.getAccelerationStructure();  // So that we can take its address
   descriptorAS.accelerationStructureCount = 1;
   descriptorAS.pAccelerationStructures    = &tlasCopy;
-  writeDescriptorSets[1]                  = descriptorSetContainer.makeWrite(0, BINDING_TLAS, &descriptorAS);
+  writeDescriptorSets[1]                  = descriptorSetContainer.makeWrite(0, binding_tlas, &descriptorAS);
   // Vertex buffer
   VkDescriptorBufferInfo vertexDescriptorBufferInfo{};
   vertexDescriptorBufferInfo.buffer = vertexBuffer.buffer;
   vertexDescriptorBufferInfo.range  = VK_WHOLE_SIZE;
-  writeDescriptorSets[2] = descriptorSetContainer.makeWrite(0, BINDING_VERTICES, &vertexDescriptorBufferInfo);
+  writeDescriptorSets[2] = descriptorSetContainer.makeWrite(0, binding_vertices, &vertexDescriptorBufferInfo);
   // Index buffer
   VkDescriptorBufferInfo indexDescriptorBufferInfo{};
   indexDescriptorBufferInfo.buffer = indexBuffer.buffer;
   indexDescriptorBufferInfo.range  = VK_WHOLE_SIZE;
-  writeDescriptorSets[3]           = descriptorSetContainer.makeWrite(0, BINDING_INDICES, &indexDescriptorBufferInfo);
+  writeDescriptorSets[3]           = descriptorSetContainer.makeWrite(0, binding_indices, &indexDescriptorBufferInfo);
   vkUpdateDescriptorSets(context,                                            // The context
                          static_cast<uint32_t>(writeDescriptorSets.size()),  // Number of VkWriteDescriptorSet objects
                          writeDescriptorSets.data(),                         // Pointer to VkWriteDescriptorSet objects
                          0, nullptr);  // An array of VkCopyDescriptorSet objects (unused)
 
   // Shader loading and pipeline creation
-  VkShaderModule rayTraceModule =
-      nvvk::createShaderModule(context, nvh::loadFile("shaders/raytrace.comp.glsl.spv", true, searchPaths));
+  VkShaderModule rayTraceModule = nvvk::createShaderModule(context, 
+    __spirv_data, __spirv_size / 4);
 
   // Describes the entrypoint and the stage to use for this shader module in the pipeline
   VkPipelineShaderStageCreateInfo shaderStageCreateInfo = nvvk::make<VkPipelineShaderStageCreateInfo>();
   shaderStageCreateInfo.stage                           = VK_SHADER_STAGE_COMPUTE_BIT;
   shaderStageCreateInfo.module                          = rayTraceModule;
-  shaderStageCreateInfo.pName                           = "main";
+  shaderStageCreateInfo.pName                           = @spirv(compute_shader);
 
   // Create the compute pipeline
   VkComputePipelineCreateInfo pipelineCreateInfo = nvvk::make<VkComputePipelineCreateInfo>();
@@ -269,8 +475,8 @@ int main(int argc, const char** argv)
                      &pushConstants);                         // Data
 
   // Run the compute shader with enough workgroups to cover the entire buffer:
-  vkCmdDispatch(cmdBuffer, (pushConstants.render_width + WORKGROUP_WIDTH - 1) / WORKGROUP_WIDTH,
-                (pushConstants.render_height + WORKGROUP_HEIGHT - 1) / WORKGROUP_HEIGHT, 1);
+  vkCmdDispatch(cmdBuffer, (pushConstants.resolution.x + workgroup_width - 1) / workgroup_width,
+                (pushConstants.resolution.y + workgroup_height - 1) / workgroup_height, 1);
 
   // Add a command that says "Make it so that memory writes by the compute shader
   // are available to read from the CPU." (In other words, "Flush the GPU caches
@@ -292,7 +498,8 @@ int main(int argc, const char** argv)
 
   // Get the image data back from the GPU
   void* data = allocator.map(buffer);
-  stbi_write_hdr("out.hdr", pushConstants.render_width, pushConstants.render_height, 3, reinterpret_cast<float*>(data));
+  stbi_write_hdr("out.hdr", pushConstants.resolution.x, pushConstants.resolution.y, 
+    3, reinterpret_cast<float*>(data));
   allocator.unmap(buffer);
 
   vkDestroyPipeline(context, computePipeline, nullptr);
